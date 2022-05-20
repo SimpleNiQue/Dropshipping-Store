2bbd16d23f9cfb2305652c7f0e601313c97bd298
simplenick <simplenick01@gmail.com>
2022-05-20 12:38:05 +0100

    added models to admin

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Customer, Item, Order
 
-# Register your models here.
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(Customer)
+admin.site.register(Category)
+
