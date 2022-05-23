98e0223f09f9dbe2e1cc45591b7d0bd157f262d4
simplenick <simplenick01@gmail.com>
2022-05-23 03:45:40 +0100

    added cart but not yet working or migrated

--- market/models.py
+++ market/models.py
@@ -29,6 +29,31 @@ class Item(models.Model):
     verbose_name_plural = 'Items'
 
 
+class Size(models.Model):
+  """Contains Every Size Available"""
+  size_name = models.CharField(max_length=200, null=True,)
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    verbose_name_plural = 'Sizes'
+
+class Cart(models.Model):
+  #TODO: Complete it later
+  """Contains all The User's selected Items"""
+  name = models.CharField(max_length=200, null=True)
+  price = models.CharField(max_length=200, null=True)
+  size = models.ForeignKey(Size, null=True, on_delete=models.SET_NULL)
+  item_img = models.ImageField(upload_to = "images/")
+  date_added = models.DateTimeField(auto_now_add=True, null=True)
+  
+  def __str__(self):
+    return self.name
+  class Meta:
+    verbose_name_plural = 'Carts'
+
+
 class Order(models.Model):
   STATUS = ("Pending", "Delivered")
     
